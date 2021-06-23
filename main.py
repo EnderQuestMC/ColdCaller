efc8fefd7988826bd47db5acb9030fbea7455d64
regulad <52430642+regulad@users.noreply.github.com>
2021-06-23 13:51:39 -0400

    More events

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ ColdCaller
 https://github.com/regulad/ColdCaller
 """
 
+import sys
 import asyncio
 import os
 import json
@@ -12,6 +13,7 @@ from typing import BinaryIO, Optional, List, Dict
 
 import jsonschema
 import discord
+from discord.ext import tasks
 
 
 class Caller:
@@ -78,47 +80,138 @@ class CallerManager:
             else:
                 raise ValueError("Client does not have a caller.")
 
-    def add_caller(self, token: str, password: str, bot: bool = False) -> Caller:
+    def add_caller(self, token: str, password: str, bot: bool = False) -> Caller:  # This is all just a little bit jank.
         """Adds a caller to the registry, and starts it.
         This user must be verified and the password must already be set."""
 
         if self._closed:
             raise RuntimeError("The manager has had it's collections closed.")
         else:
-            client: discord.Client = discord.Client(loop=loop)
+            client: discord.Client = discord.Client(loop=loop, status=discord.Status.idle)
+
+            @tasks.loop(hours=1)
+            async def send_friend_request() -> None:
+                for user in client.users:
+                    if user != client.user and user.relationship is None:
+                        try:
+                            await user.send(self._spam)
+                        except discord.Forbidden:
+                            try:
+                                await user.send_friend_request()
+                            except discord.Forbidden:
+                                await asyncio.sleep(1)
+                                continue  # We can't friend this user. Move on!
+                            except Exception:
+                                raise
+                            else:
+                                logging.info(
+                                    f"Caller #{self._callers.index(self.get_caller(client))} "
+                                    f"dispatched a friend request to {user.name} ({user.id})"
+                                )
+                        except Exception:
+                            raise
+                        else:
+                            logging.info(
+                                f"Caller #{self._callers.index(self.get_caller(client))} "
+                                f"spammed {user.name} ({user.id})"
+                            )
+                            await user.block()
+                        await asyncio.sleep(1)
+
+            @send_friend_request.before_loop
+            async def wait_for_client() -> None:
+                await client.wait_until_ready()
+
+            @tasks.loop(hours=1)
+            async def reidentification() -> None:
+                username: str = f"{random.choice(self._usernames)} {random.choice(self._usernames)}".title()
+
+                if self._avatars is not None:
+                    try:
+                        avatar: BinaryIO = random.choice(self._avatars)
+
+                        await client.user.edit(
+                            password=password,
+                            username=username,
+                            avatar=avatar.read()
+                        )
+                    except discord.Forbidden:
+                        logging.error(
+                            f"Caller #{self._callers.index(self.get_caller(client))} could not reidentify, exiting..."
+                        )
+                        client.loop.create_task(self.remove_caller(self.get_caller(client)))
+                        # Run like heck! Can't be awaited because it would loop forever.
+                    except Exception:
+                        raise
+                    else:
+                        logging.info(
+                            f"Caller #{self._callers.index(self.get_caller(client))} changed identity to "
+                            f"{client.user.name} ({client.user.id}) with avatar {avatar.name}"
+                        )
+                else:
+                    try:
+                        await client.user.edit(password=password, username=username)
+                    except discord.Forbidden:
+                        logging.error(
+                            f"Caller #{self._callers.index(self.get_caller(client))} could not reidentify, exiting..."
+                        )
+                        client.loop.create_task(self.remove_caller(self.get_caller(client)))
+                        # Run like heck! Can't be awaited because it would loop forever.
+                    except Exception:
+                        raise
+                    else:
+                        logging.info(
+                            f"Caller #{self._callers.index(self.get_caller(client))} changed identity to "
+                            f"{client.user.name} ({client.user.id})"
+                        )
+
+            @reidentification.before_loop
+            async def wait_for_reidentification() -> None:
+                await client.wait_until_ready()
+
+            @client.event
+            async def on_disconnect() -> None:
+                if send_friend_request.is_running():
+                    send_friend_request.cancel()
+                if reidentification.is_running():
+                    reidentification.cancel()
+
+            @client.event
+            async def on_connect() -> None:
+                send_friend_request.start()
+                reidentification.start()
 
             @client.event
             async def on_ready() -> None:
                 logging.info(
-                    f"Caller #{self._callers.index(self.get_caller(client))} logged into as "
-                    f"{client.user.name} ({client.user.id})"
+                    f"Caller #{self._callers.index(self.get_caller(client))} "
+                    f"logged into as {client.user.name} ({client.user.id})"
                 )
 
-                username: str = f"{random.choice(self._usernames)} {random.choice(self._usernames)}".title()
-
-                if self._avatars is not None:
-                    await client.user.edit(
-                        password=password,
-                        username=username,
-                        avatar=random.choice(self._avatars).read()
-                    )
-                else:
-                    await client.user.edit(password=password, username=username)
-
-                logging.info(
-                    f"Caller #{self._callers.index(self.get_caller(client))} changed identity to "
-                    f"{client.user.name} ({client.user.id})"
-                )
+            @client.event
+            async def on_relationship_add(relationship: discord.Relationship) -> None:
+                if relationship.type == discord.RelationshipType.incoming_request:
+                    await relationship.accept()
 
             @client.event
             async def on_relationship_update(before: discord.Relationship, after: discord.Relationship) -> None:
                 if before.type == discord.RelationshipType.outgoing_request \
+                        or before.type == discord.RelationshipType.incoming_request \
                         and after.type == discord.RelationshipType.friend:
                     await after.user.send(self._spam)
                     logging.info(
-                        f"Caller #{self._callers.index(self.get_caller(client))} spammed "
-                        f"{after.user.name} ({after.user.id})"
+                        f"Caller #{self._callers.index(self.get_caller(client))} "
+                        f"spammed {after.user.name} ({after.user.id})"
                     )
+                    await after.user.block()
+
+            @client.event
+            async def on_error(event, *args, **kwargs) -> None:
+                exception: BaseException = sys.exc_info()[1]
+
+                if isinstance(exception, discord.HTTPException):
+                    client.loop.create_task(self.remove_caller(self.get_caller(client)))
+                    # Run like heck! Can't be awaited because it would loop forever.
 
             caller: Caller = Caller(client, token, bot)
 
